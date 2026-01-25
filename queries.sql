c0d6ec808db8837494eb8a2aaf04087046a4e04f
Bablu123-Ray <250670@Softwarica.edu.np>
2026-01-25 19:16:58 +0545

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,12 @@
+-- VERIFICATION QUERY --
+-- This joins all normalized tables to reconstruct original data --
+
+SELECT 
+    s.StudentID, s.Name, s.Email, s.Major, 
+    m.Advisor, 
+    c.CourseID, c.CourseTitle, c.Credits, 
+    e.Grade, c.Building, c.Room
+FROM Students s
+JOIN Majors m ON s.Major = m.Major
+JOIN Enrollments e ON s.StudentID = e.StudentID
+JOIN Courses c ON e.CourseID = c.CourseID;
